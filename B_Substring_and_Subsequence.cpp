9e51ede65fc7e6127bb0bee1badbb57a88b9d9ee
S M Faisal <smf48406@gmail.com>
2024-08-10 12:44:02 +0600

    w

--- B_Substring_and_Subsequence.cpp
+++ B_Substring_and_Subsequence.cpp
@@ -17,49 +17,18 @@ typedef long double LD;
     }
 using namespace std;
 
-int longestCommonSubsequence(string &a, string &b) {
-    int m = a.size(), n = b.size();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-    return dp[m][n];
-}
-
-int longestCommonSubstring(string &a, string &b) {
-    int m = a.size(), n = b.size();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-    int maxLength = 0;
-
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                maxLength = max(maxLength, dp[i][j]);
-            } else {
-                dp[i][j] = 0;
-            }
-        }
-    }
-    return maxLength;
+int accumulate(vector<int> &arr) {
+    int n = arr.size(), sum = 0;
+    for (int i = 0; i < n; i++) sum += arr[i];
+    return sum;
 }
 
 void solve()
 {
-    string a, b; cin >> a >> b;
-    int lcsLength = longestCommonSubsequence(a, b);
-    int lcstLength = longestCommonSubstring(a, b);
-    cout << lcsLength << " " << lcstLength << endl;
-    int result1 = a.size() + b.size() - lcsLength;
-    int result2 = a.size() + b.size() - lcstLength;
-    cout << max(result1, result2) << endl;
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
 }
 
 int32_t main()
